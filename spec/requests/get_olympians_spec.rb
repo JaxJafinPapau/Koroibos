3e6b6fa4e2fce0bd6bcd66d4c655498f3a289c4b
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 22:02:25 -0600

    Robustifies test for happy path.

--- spec/requests/get_olympians_spec.rb
+++ spec/requests/get_olympians_spec.rb
@@ -15,5 +15,13 @@ describe "GET api/v1/olympians" do
         get '/api/v1/olympians'
 
         expect(response.status).to eq(200)
+
+        olympians = JSON.parse(response.body)
+        
+        expect(olympians).to be_a(Hash)
+        expect(olympians['olympians'].count).to eq(5)
+        expect(olympians['olympians'].first['total_medals_won']).to eq(1)
+        expect(olympians['olympians'].second['total_medals_won']).to eq(0)
+        expect(olympians['olympians'].sample['sport']).to eq('Underwater Basket Weaving')
     end
 end
