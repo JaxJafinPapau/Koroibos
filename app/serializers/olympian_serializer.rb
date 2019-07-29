b6b7c782260952e297bc848ce38dcfbb117fe76d
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 21:56:49 -0600

    Creates olympian serializer. Need to remove fast_jsonapi as a dependency as it nested too many hashes.

---
+++ app/serializers/olympian_serializer.rb
@@ -0,0 +1,20 @@
+class OlympianSerializer
+    def initialize(athlete)
+        @id = athlete[:id]
+        @name = athlete[:name]
+        @team = athlete[:team]
+        @age = athlete[:age]
+        @sport = athlete[:sport]
+        @total_medals_won = athlete.total_medals_won
+    end
+
+    def serialize
+        { id: @id,
+          name: @name,
+          team: @team,
+          age: @age,
+          sport: @sport,
+          total_medals_won: @total_medals_won
+        }
+    end
+end
