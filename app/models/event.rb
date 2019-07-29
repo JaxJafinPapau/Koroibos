bebbd727647c47a7481bded8daab7bf1d9534a56
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 18:09:56 -0600

    Adds presence validation and test to even name. :

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
     has_many :athlete_events
     has_many :athletes, through: :athlete_events
+
+    validates :name, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Event, type: :model do
   describe 'Relationships' do
     it { should have_many(:athletes).through(:athlete_events)}
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+  end
 end
