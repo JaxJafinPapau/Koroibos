8b6652ed5c4acbb5665a41e84ab2926b70e1ec0a
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 21:28:38 -0600

    Adds medal_count instance method test to athlete_spec.

--- spec/models/athlete_spec.rb
+++ spec/models/athlete_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Athlete, type: :model do
     it {should validate_presence_of :name}
     it {should validate_uniqueness_of :name}
   end
+
+  describe 'Instance Methods' do
+    it '.medal_count' do
+      create_list(:athlete, 2)
+      create_list(:event, 3)
+
+      winner = Athlete.first
+      loser = Athlete.second
+      winner.events << Event.first
+      winner.events << Event.second
+      loser.events << Event.third
+      winner.athlete_events.first.update(medal: 1)
+
+      expect(winner.medal_count).to eq(1)
+      expect(loser.medal_count).to eq(0)
+    end
+  end
 end
