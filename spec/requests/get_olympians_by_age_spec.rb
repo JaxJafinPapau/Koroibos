3641e8998ec4d1e6a303b345e06642ab91fc3770
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 23:39:29 -0600

    Adds testing for oldest path, controller already handling.

--- spec/requests/get_olympians_by_age_spec.rb
+++ spec/requests/get_olympians_by_age_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "GET api/v1/olympians" do
-    it 'should return all the athletes in the specified format' do
+describe "GET api/v1/olympians?age=????" do
+    it 'should return the youngest athlete when age=youngest' do
         create(:athlete, age: 3)
         create(:athlete, age: 4)
         create(:athlete, age: 96)
@@ -23,6 +23,31 @@ describe "GET api/v1/olympians" do
         expect(olympian).to be_a(Hash)
         expect(olympian['olympians'].first['id']).to eq(winner.id)
         expect(olympian['olympians'].first['age']).to eq(3)
+        expect(olympian['olympians'].first['sport']).to eq('Underwater Basket Weaving')
+    end
+
+    it 'should return the oldest athlete when age=oldest' do
+        create(:athlete, age: 3)
+        create(:athlete, age: 4)
+        create(:athlete, age: 96)
+
+        create_list(:event, 4)
+
+        winner = Athlete.where(age: 96).first
+        winner.events << Event.first
+        winner.events << Event.second
+
+        AthleteEvent.first.update(medal: 1)
+
+        get '/api/v1/olympians?age=oldest'
+
+        expect(response.status).to eq(200)
+
+        olympian = JSON.parse(response.body)
+        
+        expect(olympian).to be_a(Hash)
+        expect(olympian['olympians'].first['id']).to eq(winner.id)
+        expect(olympian['olympians'].first['age']).to eq(96)
         expect(olympian['olympians'].first['sport']).to eq('Underwater Basket Weaving')
     end
 
