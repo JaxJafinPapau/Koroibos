2f7d5e7be482eb632f0b183259a26049097f34ab
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-28 18:12:10 -0600

    Adds uniqueness and name validations and tests to athlete.

--- spec/models/athlete_spec.rb
+++ spec/models/athlete_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Athlete, type: :model do
   describe 'Relationships' do
     it { should have_many(:events).through(:athlete_events)}
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_uniqueness_of :name}
+  end
 end
