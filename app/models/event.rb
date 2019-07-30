b501862f67f5e8474bed54535f6ce1a9341430d7
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-30 12:32:09 -0600

    Creates find_by_sport class method on Event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
     has_many :athletes, through: :athlete_events
 
     validates :name, presence: true, uniqueness: true
+
+    def self.find_by_sport(sport)
+        where("events.name LIKE ?", "%#{sport}%").pluck(:name)
+    end
 end
