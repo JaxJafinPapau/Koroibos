fe129ea04aea2ae02e8c14c5284f7bbf094335ef
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-30 16:34:44 -0600

    Refactors if & elsif statement into 1 class method call with param. Gets DB call into model, not controller.

--- app/controllers/api/v1/athletes_controller.rb
+++ app/controllers/api/v1/athletes_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::AthletesController < ApplicationController
     def index
-        if params['age'] == "youngest"
-            olympians = Athlete.order(age: :asc).limit(1).map { |a| serialize_athlete(a) }
-        elsif params['age'] == "oldest"
-            olympians = Athlete.order(age: :desc).limit(1).map { |a| serialize_athlete(a) }
+        if params['age']
+            olympians = Athlete.oldest_or_youngest(params['age']).map { |o| serialize_athlete(o) }
+            # olympians = Athlete.order(age: :desc).limit(1).map { |a| serialize_athlete(a) }
         elsif !params['age']
-            olympians = Athlete.with_total_medals_won
+            olympians = Athlete.with_total_medals_won.map { |o| serialize_athlete(o) }
         end
         render status: 200, json: { olympians: olympians } if olympians.first
         render status: 404, json: { error: "No Olympians found."} if !olympians.first
