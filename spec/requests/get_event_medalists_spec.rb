ccdb30e5e56aac5e3e73456a2d8bba648478e847
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-30 20:26:38 -0600

    Adds sad path testing to get event medalists request spec.

--- spec/requests/get_event_medalists_spec.rb
+++ spec/requests/get_event_medalists_spec.rb
@@ -31,4 +31,34 @@ describe 'GET /api/v1/events/:id/medalists' do
         expect(medalists['medalists']).to be_a(Array)
         expect(medalists['medalists'].first['medal']).to_not eq("na")
     end
+
+    #sad path
+    it 'should return the proper error message when event is not found' do
+        create_list(:athlete, 5)
+
+        create_list(:event, 4)
+
+        winner_1 = Athlete.first
+        winner_1.events << Event.first
+        winner_2 = Athlete.second
+        winner_2.events << Event.first
+        loser_2 = Athlete.third
+        loser_2.events << Event.first; loser_2.events << Event.third
+        loser_3 = Athlete.fourth
+        loser_3.events << Event.second; loser_3.events << Event.third
+        loser_4 = Athlete.fifth
+        loser_4.events << Event.second; loser_4.events << Event.third
+
+        winner_1.athlete_events.first.update(medal: 1)
+        winner_2.athlete_events.first.update(medal: 2)
+
+        event_id = Event.last.id + 2008
+        get "/api/v1/events/#{event_id}/medalists"
+
+        expect(response.status).to eq(404)
+
+        error = JSON.parse(response.body)
+
+        expect(error['error']).to eq("Event not found.")
+    end
 end
